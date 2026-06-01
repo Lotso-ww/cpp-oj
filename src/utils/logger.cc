95ea16cc7ac4f5b860ee22d7c3d499cef11d3563
Lotso <3208137151@qq.com>
2026-06-01 22:28:59 +0800

    日志类初步实现

---
+++ src/utils/logger.cc
@@ -0,0 +1,148 @@
+#include "logger.h"
+#include "config.h"
+
+namespace LogModule
+{
+
+std::string LogLevel2String(LogLevel level)
+{
+    switch (level)
+    {
+    case LogLevel::DEBUG:
+        return "DEBUG";
+    case LogLevel::INFO:
+        return "INFO";
+    case LogLevel::WARNING:
+        return "WARNING";
+    case LogLevel::ERROR:
+        return "ERROR";
+    case LogLevel::FATAL:
+        return "FATAL";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+std::string GetTimeStamp()
+{
+    time_t currentTime = time(nullptr);
+    struct tm dataTime;
+    localtime_r(&currentTime, &dataTime);
+
+    char dataTimeStr[128];
+    snprintf(dataTimeStr, sizeof(dataTimeStr), "%4d-%02d-%02d %02d:%02d:%02d",
+             dataTime.tm_year + 1900,
+             dataTime.tm_mon + 1,
+             dataTime.tm_mday,
+             dataTime.tm_hour,
+             dataTime.tm_min,
+             dataTime.tm_sec);
+    return dataTimeStr;
+}
+
+ConsoleLogStrategy::ConsoleLogStrategy() {}
+
+ConsoleLogStrategy::~ConsoleLogStrategy() {}
+
+void ConsoleLogStrategy::SyncLog(const std::string &message)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    std::cout << message << std::endl;
+}
+
+FileLogStrategy::FileLogStrategy(const std::string &logfilepath)
+    : _logfilepath(logfilepath)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    std::ofstream out(_logfilepath, std::ios::app);
+    (void)out;
+}
+
+FileLogStrategy::~FileLogStrategy() {}
+
+void FileLogStrategy::SyncLog(const std::string &message)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    std::ofstream out(_logfilepath, std::ios::app);
+    if (!out.is_open())
+    {
+        return;
+    }
+    out << message << "\n";
+    out.close();
+}
+
+Logger &Logger::getInstance()
+{
+    static Logger instance;
+    return instance;
+}
+
+void Logger::UseConsoleLogStrategy()
+{
+    _strategy = std::make_unique<ConsoleLogStrategy>();
+}
+
+void Logger::UseFileLogStrategy(const std::string &logfilepath)
+{
+    _strategy = std::make_unique<FileLogStrategy>(logfilepath);
+}
+
+Logger::LogMessage::LogMessage(LogLevel level, const std::string &filename, int line, Logger &self)
+    : _currenttime(GetTimeStamp()),
+      _loglevel(LogLevel2String(level)),
+      _pid(getpid()),
+      _filename(filename),
+      _line(line),
+      _logger(self)
+{
+    std::stringstream ss;
+    ss << "[" << _currenttime << "] "
+       << "[" << _loglevel << "] "
+       << "[" << _pid << "] "
+       << "[" << _filename << "] "
+       << "[" << _line << "] "
+       << "- ";
+    _loginfo = ss.str();
+}
+
+Logger::LogMessage::~LogMessage()
+{
+    if (_logger._strategy)
+    {
+        _logger._strategy->SyncLog(_loginfo);
+    }
+}
+
+template <typename T>
+Logger::LogMessage &Logger::LogMessage::operator<<(const T &info)
+{
+    std::stringstream ss;
+    ss << info;
+    _loginfo += ss.str();
+    return *this;
+}
+
+Logger::LogMessage Logger::operator()(LogLevel level, const std::string &filename, int line)
+{
+    return LogMessage(level, filename, line, *this);
+}
+
+void InitLogger()
+{
+    auto &config = oj::Config::getInstance();
+    std::string logFile = config.getLogFile();
+
+    if (!logFile.empty())
+    {
+        logger.UseFileLogStrategy(logFile);
+    }
+    else
+    {
+        logger.UseConsoleLogStrategy();
+    }
+}
+
+Logger &logger = Logger::getInstance();
+
+}
---
+++ src/utils/logger.h
@@ -0,0 +1,100 @@
+#ifndef OJ_LOGGER_H
+#define OJ_LOGGER_H
+
+#include <string>
+#include <memory>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <unistd.h>
+#include <mutex>
+
+namespace LogModule
+{
+
+enum class LogLevel
+{
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+};
+
+std::string LogLevel2String(LogLevel level);
+std::string GetTimeStamp();
+
+class LogStrategy
+{
+public:
+    virtual ~LogStrategy() = default;
+    virtual void SyncLog(const std::string &message) = 0;
+};
+
+class ConsoleLogStrategy : public LogStrategy
+{
+public:
+    ConsoleLogStrategy();
+    ~ConsoleLogStrategy() override;
+    void SyncLog(const std::string &message) override;
+
+private:
+    std::mutex _mutex;
+};
+
+class FileLogStrategy : public LogStrategy
+{
+public:
+    explicit FileLogStrategy(const std::string &logfilepath);
+    ~FileLogStrategy() override;
+    void SyncLog(const std::string &message) override;
+
+private:
+    std::string _logfilepath;
+    std::mutex _mutex;
+};
+
+class Logger
+{
+public:
+    static Logger &getInstance();
+
+    void UseConsoleLogStrategy();
+    void UseFileLogStrategy(const std::string &logfilepath);
+
+    class LogMessage
+    {
+    public:
+        LogMessage(LogLevel level, const std::string &filename, int line, Logger &self);
+        ~LogMessage();
+
+        template <typename T>
+        LogMessage &operator<<(const T &info);
+
+    private:
+        std::string _currenttime;
+        std::string _loglevel;
+        int _pid;
+        std::string _filename;
+        int _line;
+        std::string _loginfo;
+        Logger &_logger;
+    };
+
+    LogMessage operator()(LogLevel level, const std::string &filename, int line);
+    std::unique_ptr<LogStrategy> _strategy;
+};
+
+extern Logger &logger;
+
+#define LOG(level) logger.getInstance()(level, __FILE__, __LINE__)
+
+#define ENABLE_CONSOLE_LOG_STRATEGY() logger.UseConsoleLogStrategy()
+#define ENABLE_FILE_LOG_STRATEGY(logfilepath) logger.UseFileLogStrategy(logfilepath)
+
+void InitLogger();
+
+}
+
+#endif
